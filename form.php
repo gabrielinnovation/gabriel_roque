2e86a27ace29aca4bc804b6a8ddfa645c504d493
Gabriel Roque <132180633+gabrielinnovation@users.noreply.github.com>
2023-11-12 09:50:19 -0300

    Update form.php

--- form.php
+++ form.php
@@ -6,15 +6,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $celular = $_POST["celular"];
     $mensagem = $_POST["mensagem"];
 
-    // Aqui você pode realizar ações com os dados, como salvá-los em um banco de dados ou enviá-los por email
-    // Por exemplo, exibindo na tela para este exemplo
-    echo "Nome: " . $nome . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Celular: " . $celular . "<br>";
-    echo "Mensagem: " . $mensagem . "<br>";
+    // Configurações de email
+    $destinatario = "gabrieltechvision@gmail.com"; // Substitua pelo seu endereço de email
+    $assunto = "Nova mensagem do formulário";
+
+    // Corpo do email
+    $corpo = "Nome: $nome\n";
+    $corpo .= "Email: $email\n";
+    $corpo .= "Celular: $celular\n";
+    $corpo .= "Mensagem: $mensagem\n";
+
+    // Envia o email
+    $enviado = mail($destinatario, $assunto, $corpo);
+
+    // Verifica se o email foi enviado com sucesso
+    if ($enviado) {
+        echo "Obrigado por entrar em contato! Sua mensagem foi enviada.";
+    } else {
+        echo "Desculpe, houve um problema no envio da mensagem. Por favor, tente novamente mais tarde.";
+    }
 } else {
     // Se alguém tentar acessar este script diretamente, redireciona para o formulário
     header("Location: formulario.html");
     exit();
 }
 ?>
+
